e273151a6452c0885899cb0206d2161de8016bd9
commenthol <commenthol@gmail.com>
2015-12-03 12:55:23 -1000

    julian and orthodox easter

--- README.md
+++ README.md
@@ -1,17 +1,28 @@
-# dates-easter
+# date-easter
 
 > Calculates Easter for a given year
 
 ## Synopsis
 
 ````js
-var easter = require('dates-easter')
-easter.easter(2015)     // new Date('2016-03-27')
-easter.gregorianEaster(new Date('2015-01-01'))
-                        // new Date('2016-03-27')
+var easter = require('date-easter')
+easter.easter(2016)
+// { year: 2016, month: 3, day: 27 }
+easter.gregorianEaster(new Date('2016-01-01'))
+// { year: 2016, month: 3, day: 27 }
+easter.julianEaster(2016)
+// { year: 2016, month: 4, day: 18 }
+easter.orthodoxEaster(2016)
+// { year: 2016, month: 5, day: 1 }
 ````
 
 ## Description
+
+All methods return an object with the following properties
+- **year**: Year (begins at 0)
+- **month**: Month (1 ... 12)
+- **day**: Day (1 ... 31)  
+- **toString()**: Returns a formatted String `YYYY-MM-DD`
 
 ### easter
 
@@ -19,13 +30,21 @@ Shortcut for `gregorianEaster`.
 
 ### gregorianEaster
 
-Returns the date of Gregorian Easter for a given year.
+Returns the date of Gregorian Easter for a given year. Uses the extended [Gaussian Easter algorithm][].
+
+### julianEaster
+
+Returns the date of Julian Easter for a given year in the Julian Calender
+
+### orthodoxEaster
+
+Returns the date of Julian Easter for a given year in the Gregorian Calender
 
 ## References
 
 - Date::Easter Perl Package <https://metacpan.org/pod/Date::Easter> Rich Bowen <rbowen@rcbowen.com>
 - <http://www.chariot.net.au/~gmarts/eastcalc.htm>
-- <https://de.wikipedia.org/wiki/Gau%C3%9Fsche_Osterformel>
+- [Gaussian Easter algorithm][]
 
 ## License
 
@@ -34,3 +53,4 @@ Copyright (c) 2015 commenthol
 Software is released under [MIT][license].
 
 [license]: ./LICENSE
+[Gaussian Easter algorithm]: https://de.wikipedia.org/wiki/Gau%C3%9Fsche_Osterformel#Eine_erg.C3.A4nzte_Osterformel
--- index.js
+++ index.js
@@ -13,23 +13,42 @@
     if (!year) {
       year = new Date().getYear()
     } else if (year instanceof Date) {
-      year = year.getYear()
+      year = year.getYear() + 1900
     }
     return year
   }
 
+  function EasterDate (year, month, day) {
+    this.year  = year
+    this.month = month
+    this.day   = day
+  }
+  
+  EasterDate.prototype = {
+    toString: function () {
+      function pre (num, l) {
+        l = l || 2
+        var s = '0000' + num
+        return s.substr(s.length - l, l)
+      } 
+      return [pre(this.year, 4), pre(this.month), pre(this.day)].join('-')
+    }
+  }
+
   /**
-   * calculate easter sunday in the gregorian calendar
-   * @param {Date|Number} year
    * from https://de.wikipedia.org/wiki/Gau%C3%9Fsche_Osterformel
    * ergänzte Formel
    */
-  function gregorianEaster (year) {
+  function _easter (year, julian, gregorian) {
     year = _toYear(year)
 
     var k = Math.floor(year / 100)
     var m = 15 + Math.floor((3 * k + 3) / 4) - Math.floor((8 * k + 13) / 25)
     var s = 2 - Math.floor((3 * k + 3) / 4)
+    if (julian) {
+      m = 15
+      s = 0
+    }
     var a = year % 19
     var d = (19 * a + m) % 30
     var r = Math.floor((d + a / 11) / 29)
@@ -37,7 +56,29 @@
     var sz = 7 - Math.floor(year + year / 4 + s) % 7
     var oe = 7 - (og - sz) % 7
     var os = og + oe
-    return new Date(year, 2, os)
+    if (gregorian) {      
+      os = os + Math.floor(year / 100) - Math.floor(year / 400) - 2
+    }
+    //                      1   2   3   4   5   6   7   8
+    var daysPerMonth = [0, 31, 28, 31, 30, 31, 30, 31, 31]
+    var day = os
+    var month
+    for (month = 3; month < 8; month++) {
+      if (day <= daysPerMonth[month]) {
+        break
+      }
+      day -= daysPerMonth[month]
+    }
+    
+    return new EasterDate(year, month, day)
+  }
+
+  /**
+   * calculate easter sunday in the gregorian calendar
+   * @param {Date|Number} year
+   */
+  function gregorianEaster (year) {
+    return _easter(year)
   }
   exports.gregorianEaster = gregorianEaster
 
@@ -47,6 +88,25 @@
    * @param {Date|Number} year
    */
   exports.easter = gregorianEaster
+
+  /**
+   * calculate easter sunday in the julian calendar
+   * @param {Date|Number} year
+   * @param {Boolean} [gregorian] - if `true` then output is a date in the gregorian calendar
+   */
+  function julianEaster (year) {
+    return _easter(year, true)
+  }
+  exports.julianEaster = julianEaster
+
+  /**
+   * Orthodox Easter in gregorian calender
+   * @param {Date|Number} year
+   */
+  function orthodoxEaster (year) {
+    return _easter(year, true, true)
+  }
+  exports.orthodoxEaster = orthodoxEaster
 
   // Node.js
   if (typeof ctx.Window === 'undefined' && typeof module !== 'undefined' && module.exports) {
